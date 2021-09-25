d913e552303f95dd2e8b39c3492b2577bed6583d
Gustavo Costa <gustavomurilo.costa@hotmail.com>
2021-09-25 19:27:23 -0300

    Update

--- ProjetoIntegrador/src/ProjetoInt/Aluno.java
+++ ProjetoIntegrador/src/ProjetoInt/Aluno.java
@@ -4,52 +4,90 @@ import java.util.ArrayList;
 
 public class Aluno {
 
-	public ArrayList<Integer> IdAluno = new ArrayList<Integer>();
+	public ArrayList<String> IdAluno = new ArrayList<String>();
 	public ArrayList<String> Nome = new ArrayList<String>();
-	public ArrayList<Integer> dataAluno = new ArrayList<Integer>();
-	public ArrayList<Integer> cpfAluno = new ArrayList<Integer>();
+	public ArrayList<String> dataAluno = new ArrayList<String>();
+	public ArrayList<String> cpfAluno = new ArrayList<String>();
+	public ArrayList<String> tutorAluno = new ArrayList<String>();
+	public ArrayList<String> departamentoAluno = new ArrayList<String>();
+	public ArrayList<String> disciplinaAluno = new ArrayList<String>();
 
-	public int idAluno;
+	public String idAluno;
 	public String nome;
-	public int cpf;
-	public int dataNascimento;
+	public String cpf;
+	public String dataNascimento;
+	public String tutor;
+	public String departamento;
+	public String disciplina;
 
-	public Aluno(int idAluno, String nome, int cpf, int dataNascimento) {
+	public Aluno(String idAluno, String nome, String cpf, String dataNascimento, String tutor, String departamento,
+			String disciplina) {
 		this.idAluno = idAluno;
 		this.nome = nome;
 		this.cpf = cpf;
 		this.dataNascimento = dataNascimento;
+		this.tutor = tutor;
+		this.departamento = departamento;
+		this.disciplina = disciplina;
 	}
 
-	public void addId(int id) {
-		if (id < 0 & 5 < id) {
-		System.out.println("Erro ao incluir Id");
-		}else
-		IdAluno.add(id);
+	/////////////////////////////////////////// ADICIONA///////////////////////////////////
+	public void addId(String id) {
+		if (id.length() > 0 && 5 >= id.length()) {
+			IdAluno.add(id);
+			// Opcoes.Cadastrar();
+		} else
+			System.out.println("Erro ao incluir Id");
 	}
 
 	public void addNome(String nome) {
-		if (nome.length() < 0 || 10 < nome.length()) {
-			System.out.println("Erro ao incluir nome");
-		} else 
+		if (nome.length() > 0 && 10 >= nome.length()) {
 			Nome.add(nome);
+		} else
+			System.out.println("Erro ao incluir Nome");
 	}
 
-	public void addData(int data) {
-		if (data < 0 && 6 < data) {
-			System.out.println("Erro ao incluir data");
-		} else 
+	public void addData(String data) {
+		if (data.length() == 6) {
 			dataAluno.add(data);
+		} else
+			System.out.println("Erro ao incluir Data");
 	}
 
-	public void addCpf(int cpf) {
-		if (cpf < 0 & 11 < cpf) {
-			System.out.println("Erro ao incluir Id");
-		} else 
+	public void addCpf(String cpf) {
+		if (cpf.length() == 11) {
 			cpfAluno.add(cpf);
+		} else
+			System.out.println("Erro ao incluir CPF");
+
 	}
 
-	public void printId(int id) {
+	public void addTutor(String tutor) {
+		if (tutor.length() > 0 && 10 >= tutor.length()) {
+			tutorAluno.add(tutor);
+		} else
+			System.out.println("Erro ao incluir Tutor");
+
+	}
+
+	public void addDepartamneto(String departamento) {
+		if (departamento.length() > 0 && 15 >= departamento.length()) {
+			departamentoAluno.add(departamento);
+		} else
+			System.out.println("Erro ao incluir Departamento");
+
+	}
+
+	public void addDisciplina(String disciplina) {
+		if (disciplina.length() > 0 || 15 >= disciplina.length()) {
+			disciplinaAluno.add(disciplina);
+		} else
+			System.out.println("Erro ao incluir Disciplina");
+
+	}
+
+	////////////////////////////////////////////////// IMPRIME////////////////////////////////////////////
+	public void printId(String id) {
 		System.out.println("Id aluno:" + id);
 
 	}
@@ -58,48 +96,101 @@ public class Aluno {
 		System.out.println("Nome do Aluno:" + nome);
 	}
 
-	public void printData(int data) {
+	public void printData(String data) {
 		System.out.println("Data de nascimento do aluno:" + data);
 	}
 
-	public void printCpf(int cpf) {
+	public void printCpf(String cpf) {
 		System.out.println("Cpf do aluno:" + cpf);
 
 	}
+
+	public void printTutor(String tutor) {
+		System.out.println("Cpf do aluno:" + tutor);
+
+	}
+
+	public void printDepartamento(String departamento) {
+		System.out.println("Cpf do aluno:" + departamento);
+
+	}
+
+	public void printDisciplina(String disciplina) {
+		System.out.println("Cpf do aluno:" + disciplina);
+
+	}
+
+	//////////////////////////////////////////////// UPDATE//////////////////////////////////////////////////////////////////
 
 	public void UpdateStr(String antigo, String novo) {
 
 		int valor = Nome.indexOf(antigo);
 		Nome.set(valor, novo);
 	}
-	public void UpdateData(int antigo, int novo) {
+
+	public void UpdateData(String antigo, String novo) {
 
 		int recebe = dataAluno.indexOf(antigo);
 		dataAluno.set(recebe, novo);
 	}
-	public void UpdateCpf(int antigo, int novo) {
+
+	public void UpdateCpf(String antigo, String novo) {
 
 		int recebe = cpfAluno.indexOf(antigo);
 		cpfAluno.set(recebe, novo);
 	}
 
-	public void RemoveId(int num) {
+	public void UpdateTutor(String antigo, String novo) {
+
+		int recebe = tutorAluno.indexOf(antigo);
+		tutorAluno.set(recebe, novo);
+	}
+
+	public void UpdateDepartamento(String antigo, String novo) {
+
+		int recebe = departamentoAluno.indexOf(antigo);
+		departamentoAluno.set(recebe, novo);
+	}
+
+	public void UpdateDisciplina(String antigo, String novo) {
+
+		int recebe = disciplinaAluno.indexOf(antigo);
+		disciplinaAluno.set(recebe, novo);
+	}
+
+	//////////////////////////////////////////////// REMOVE/////////////////////////////////////////////
+	public void RemoveId(String num) {
 		int a = IdAluno.indexOf(num);
 		IdAluno.remove(a);
 	}
 
-	public void RemoveStr(String _nome) {
-		int valor = Nome.indexOf(_nome);
+	public void RemoveStr(String nome) {
+		int valor = Nome.indexOf(nome);
 		Nome.remove(valor);
 	}
 
-	public void RemoveData(int num) {
-		int a = dataAluno.indexOf(num);
+	public void RemoveData(String nome) {
+		int a = dataAluno.indexOf(nome);
 		dataAluno.remove(a);
 	}
 
-	public void RemoveCpf(int num) {
-		int a = cpfAluno.indexOf(num);
+	public void RemoveCpf(String nome) {
+		int a = cpfAluno.indexOf(nome);
 		cpfAluno.remove(a);
+	}
+
+	public void RemoveTutor(String nome) {
+		int a = tutorAluno.indexOf(nome);
+		tutorAluno.remove(a);
+	}
+
+	public void RemoveDepartamento(String nome) {
+		int a = departamentoAluno.indexOf(nome);
+		departamentoAluno.remove(a);
+	}
+
+	public void RemoveDisciplina(String nome) {
+		int a = disciplinaAluno.indexOf(nome);
+		disciplinaAluno.remove(a);
 	}
 }
--- ProjetoIntegrador/src/ProjetoInt/Opcoes.java
+++ ProjetoIntegrador/src/ProjetoInt/Opcoes.java
@@ -3,56 +3,82 @@ package ProjetoInt;
 import java.util.Scanner;
 
 public class Opcoes {
-	private int VetorIdAluno;
-	private String VetornomeAluno;
-	private int VetordataAluno;
-	private int VetorcpfAluno;
+	private String VetorIdAluno;
+	private String VetorNomeAluno;
+	private String VetorDataAluno;
+	private String VetorCpfAluno;
+	private String VetorTutorAluno;
+	private String VetorDepartamentoAluno;
+	private String VetorDisciplinaAluno;
 
-	Aluno aluno1 = new Aluno(VetorIdAluno, VetornomeAluno, VetorIdAluno, VetorIdAluno);
+	Aluno aluno1 = new Aluno(VetorIdAluno, VetorNomeAluno, VetorDataAluno, VetorCpfAluno, VetorTutorAluno,
+			VetorDepartamentoAluno, VetorDisciplinaAluno);
 
 	public Scanner teclado = new Scanner(System.in);
 
 	public void Cadastrar() {
-		System.out.print("\n");
-		System.out.println("Cadastro de aluno");
-		System.out.println("");
+		try {
+			System.out.print("\n");
+			System.out.println("Cadastro de aluno");
+			System.out.println("");
 
-		System.out.println("Id do aluno");
-		VetorIdAluno = Integer.parseInt(teclado.nextLine());
-		aluno1.addId(VetorIdAluno);
+			System.out.println("Id do aluno");
+			VetorIdAluno = teclado.nextLine();
+			aluno1.addId(VetorIdAluno);
 
-		System.out.println("Nome do aluno");
-		VetornomeAluno = teclado.nextLine();
-		aluno1.addNome(VetornomeAluno);
+			System.out.println("Nome do aluno");
+			VetorNomeAluno = teclado.nextLine();
+			aluno1.addNome(VetorNomeAluno);
 
-		System.out.println("Data de nascimento do aluno:");
-		VetordataAluno = Integer.parseInt(teclado.nextLine());
-		aluno1.addData(VetordataAluno);
+			System.out.println("Data de nascimento do aluno:");
+			VetorDataAluno = teclado.nextLine();
+			aluno1.addData(VetorDataAluno);
 
-		System.out.println("CPF do aluno");
-		VetorcpfAluno = Integer.parseInt(teclado.nextLine());
-		aluno1.addCpf(VetorcpfAluno);
+			System.out.println("CPF do aluno");
+			VetorCpfAluno = teclado.nextLine();
+			aluno1.addCpf(VetorCpfAluno);
 
+			System.out.println("Tutor do aluno");
+			VetorTutorAluno = teclado.nextLine();
+			aluno1.addTutor(VetorTutorAluno);
+
+			System.out.println("Departamento do aluno");
+			VetorDepartamentoAluno = teclado.nextLine();
+			aluno1.addDepartamneto(VetorDepartamentoAluno);
+
+			System.out.println("Disciplina do aluno");
+			VetorDisciplinaAluno = teclado.nextLine();
+			aluno1.addDisciplina(VetorDisciplinaAluno);
+
+		} catch (Exception e) {
+			System.out.println("Lista Vazia");
+		}
 	}
 
 	public void Listar() {
-			System.out.print("\n");
-			System.out.println("Alunos listados");
-			System.out.println("id:"+aluno1.IdAluno);
-            System.out.println("nome:"+aluno1.Nome);
-            System.out.println("data:"+aluno1.dataAluno);
-            System.out.println("cpf:"+aluno1.cpfAluno);
-	}
 
+		System.out.print("\n");
+		System.out.println("Alunos listados");
+		System.out.println("Id:" + aluno1.IdAluno);
+		System.out.println("Nome:" + aluno1.Nome);
+		System.out.println("Data:" + aluno1.dataAluno);
+		System.out.println("CPF:" + aluno1.cpfAluno);
+		System.out.println("Tutor:" + aluno1.tutorAluno);
+		System.out.println("Departamento:" + aluno1.departamentoAluno);
+		System.out.println("Disciplina:" + aluno1.disciplinaAluno);
+	}
 	public void Pesquisar() {
 		System.out.print("\n");
 		System.out.println("Digite o nome do Aluno");
-		String i = teclado.nextLine();
-		if (i.equals(VetornomeAluno)) {
+		String compara = teclado.nextLine();
+		if (compara.equals(VetorNomeAluno)) {
 			aluno1.printId(VetorIdAluno);
-			aluno1.printStr(VetornomeAluno);
-			aluno1.printData(VetordataAluno);
-			aluno1.printCpf(VetorcpfAluno);
+			aluno1.printStr(VetorNomeAluno);
+			aluno1.printData(VetorDataAluno);
+			aluno1.printCpf(VetorCpfAluno);
+			aluno1.printTutor(VetorTutorAluno);
+			aluno1.printDepartamento(VetorDepartamentoAluno);
+			aluno1.printDisciplina(VetorDisciplinaAluno);
 		} else {
 			System.out.println("Digite um Nome Válido");
 			Pesquisar();
@@ -60,41 +86,66 @@ public class Opcoes {
 	}
 
 	public void Atualizar() {
-		System.out.print("\n");
-		System.out.println("Digite o id do Aluno");
-		int i = teclado.nextInt();
-		if (i != VetorIdAluno) {
-			System.out.println("Digite um id Valido");
-			Atualizar();
-		} else {
-			System.out.println("Digite o nome do aluno");
-			String novoNome = teclado.next();
-			aluno1.UpdateStr(VetornomeAluno, novoNome);
-		}
-		System.out.println("Digite a data de Nascimento do aluno");
-		int novoData = teclado.nextInt();
-		aluno1.UpdateData(VetordataAluno, novoData);
+		try {
+			System.out.print("\n");
+			System.out.println("Digite o id do Aluno");
+			String compara = teclado.nextLine();
+			if (compara != VetorIdAluno) {
+				System.out.println("Digite o nome do aluno");
+				String novoNome = teclado.next();
+				aluno1.UpdateStr(VetorNomeAluno, novoNome);
+			} else {
+				System.out.println("Digite um id Valido");
+				Atualizar();
+			}
+			System.out.println("Digite a data de Nascimento do aluno");
+			String novoData = teclado.next();
+			aluno1.UpdateData(VetorDataAluno, novoData);
 
-		System.out.println("Digite o CPF do aluno");
-		int novoCpf = teclado.nextInt();
-		aluno1.UpdateCpf(VetorcpfAluno, novoCpf);
+			System.out.println("Digite o CPF do aluno");
+			String novoCpf = teclado.next();
+			aluno1.UpdateCpf(VetorCpfAluno, novoCpf);
+
+			System.out.println("Digite o Tutor do aluno");
+			String novoTutor = teclado.next();
+			aluno1.UpdateTutor(VetorTutorAluno, novoTutor);
+
+			System.out.println("Digite o Departamento do aluno");
+			String novoDepartamento = teclado.next();
+			aluno1.UpdateDepartamento(VetorDepartamentoAluno, novoDepartamento);
+
+			System.out.println("Digite a Disciplina do aluno");
+			String novoDisciplina = teclado.next();
+			aluno1.UpdateDisciplina(VetorDisciplinaAluno, novoDisciplina);
+			System.out.println("Aluno atualizado com sucesso");
+		} catch (Exception e) {
+			System.out.println("Lista Vazia");
+		}
 
 	}
 
 	public void Remover() {
-		System.out.print("\n");
-		System.out.println("Digite o id do Aluno");
-		Integer i = teclado.nextInt();
+		try {
+			System.out.print("\n");
+			System.out.println("Digite o id do Aluno");
+			String compara = teclado.nextLine();
 
-		if (i != VetorIdAluno) {
-			System.out.println("Digite um ID valido");
-			Remover();
-		} else {
-			aluno1.RemoveId(VetorIdAluno);
-			aluno1.RemoveStr(VetornomeAluno);
-			aluno1.RemoveData(VetordataAluno);
-			aluno1.RemoveCpf(VetorcpfAluno);
-			System.out.println("Aluno removido com sucesso");
+			if (compara.equals(VetorIdAluno)) {
+				aluno1.RemoveId(VetorIdAluno);
+				aluno1.RemoveStr(VetorNomeAluno);
+				aluno1.RemoveData(VetorDataAluno);
+				aluno1.RemoveCpf(VetorCpfAluno);
+				aluno1.RemoveTutor(VetorTutorAluno);
+				aluno1.RemoveDepartamento(VetorDepartamentoAluno);
+				aluno1.RemoveDisciplina(VetorDisciplinaAluno);
+				System.out.println("Aluno removido com sucesso");
+
+			} else {
+				System.out.println("Digite um ID valido");
+				Remover();
+			}
+		} catch (Exception e) {
+			System.out.println("Lista vazia");
 		}
 	}
 }
